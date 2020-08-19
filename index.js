ab87ecabb414dbb6eac430ff6bca3f282b7d57cd
ricardofisma <ricardomallqui6@gmail.com>
2020-08-18 19:54:22 -0500

    w

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-var server = require("./server.js");
-server.send();
+var express = require("express");
+var app = express;
+var port = process.env.PORT || 30000;
+const http = require("http");
+
+const hostname = "127.0.0.1";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
