05bf025893ffb7ba41d67b1a30ab17d823c90226
ricardofisma <ricardomallqui6@gmail.com>
2020-08-18 20:18:07 -0500

    w

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 var express = require("express");
-var app = express;
+var app = express();
+
 var port = process.env.PORT || 30000;
-const http = require("http");
 
-const hostname = "127.0.0.1";
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Helloww World");
+app.get("/", function (req, res) {
+  res.send("Helloww World");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port);
