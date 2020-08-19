21307e559df61d71d7bb85dad137c85375987ea4
ricardofisma <ricardomallqui6@gmail.com>
2020-08-18 20:32:13 -0500

    w

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var http = require("http");
+var express = require("express");
+var app = express();
+
 function www() {
   function onRequest(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -19,7 +21,27 @@ function www() {
     res.end();
   }
 
-  var server = http.createServer(onRequest).listen(5000);
-  console.log("5000");
+  var port = process.env.PORT || 3000;
+
+  app.get("/", function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    var ww = [
+      '<html lang="en">',
+      "<head>",
+      '<meta charset="UTF-8">',
+      '<meta name="viewport" content="width=device-width, initial-scale=1.0">',
+      "<title>Document</title>",
+      "</head>",
+      "<body>",
+      "www zzzzzzzzzzzzzz",
+      "</body>",
+      "</html>",
+    ];
+
+    res.write(ww.join(""));
+    res.end();
+  });
+
+  app.listen(port);
 }
 exports.send = www;
