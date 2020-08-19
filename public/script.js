a0f7c3c7e13bf45525add1b0320364b41c21763b
ricardofisma <ricardomallqui6@gmail.com>
2020-08-18 23:13:42 -0500

    w

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   secure: true,
-  host: "your-app-name.herokuapp.com",
+  host: "https://fisartpeers.herokuapp.com",
   port: 3001,
 });
 const myVideo = document.createElement("video");
