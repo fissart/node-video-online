0f7293292f405d940539b163daa800379915d7e7
ricardofisma <ricardomallqui6@gmail.com>
2020-08-18 20:21:35 -0500

    w

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-var express = require("express");
-var app = express();
-
-var port = process.env.PORT || 30000;
-
-app.get("/", function (req, res) {
-  res.send("Helloww World");
-});
-
-app.listen(port);
+var server = require("./server.js");
+server.send();
